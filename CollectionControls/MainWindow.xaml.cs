557bdb893384e9a17b9d97dec85b01f0f5c5f6dc
Alex <xrlogic1@gmail.com>
2024-04-26 10:22:55 +0400

    Collection Controls

---
+++ CollectionControls/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CollectionControls;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private Vehicle? _dataVehicle;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        DataContext = this;
+        
+        DataVehicle = new Vehicle
+        {
+            Id = 1,
+            Model = "Wood",
+            Name = "Quick Car"
+        };
+    }
+
+    public ObservableCollection<Vehicle> Vehicles { get; set; } = new();
+
+    public Vehicle NewVehicle { get; set; } = new();
+    
+    public Vehicle? DataVehicle
+    {
+        get => _dataVehicle;
+        set
+        {
+            _dataVehicle = value;
+            
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DataVehicle)));
+        }
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (DataVehicle == null)
+        {
+            return;
+        }
+        
+        DataVehicle.Name = "Test";
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void AddVehicle(object sender, RoutedEventArgs e)
+    {
+        Vehicles.Add(NewVehicle);
+
+        NewVehicle = new();
+    }
+}
---
+++ CollectionControls/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CollectionControls;
+
+public record Vehicle : INotifyPropertyChanged
+{
+    private string _name = null!;
+    public int Id { get; set; }
+
+    public string Name
+    {
+        get => _name;
+        set => SetField(ref _name, value);
+    }
+
+    public string Model { get; set; }  = null!;
+    
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
